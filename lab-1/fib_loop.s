9a868203b5c6f8952d381f421847a70d3d44a6d1
yodalee <lc85301@gmail.com>
2015-03-11 23:45:07 +0800

    use r4-r6 as local variable to meet AAPCS

--- lab-1/fib_loop.s
+++ lab-1/fib_loop.s
@@ -10,34 +10,32 @@
 
 fibonacci:
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r4, r5, r6, lr}
 
 	@ if(R0 <= 0) goto .L3 (which returns 0)
 	cmp r0, #0
 	ble .L3
 
-	@ r3, r4 = 0, 1, index r0
-	mov r3, #0
-	mov r4, #1
+	@ r4, r5 = 0, 1, index r0
+	mov r4, #0
+	mov r5, #1
 .LOOP:
-	mov r5, r4
-	add r4, r3, r4
-	mov r3, r5
+	mov r6, r5
+	adds r5, r4, r5
+	mov r4, r6
 
 	@ int overflow
-	cmp r4, #0
-	blt .L3
+	bvs .L3
 
-	sub r0, r0, #1
-	cmp r0, #1
-	bgt .LOOP
+	subs r0, r0, #1
+	bne .LOOP
 
 	mov r0, r4
 
-	pop {r3, r4, r5, pc}		@EPILOG
+	pop {r4, r5, r6, pc}		@EPILOG
 .L3:
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r4, r5, r6, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
